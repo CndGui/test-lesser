58d6a8725c7b31fef3380db039eeda7d42b0e974
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-03-18 23:59:05 -0300

    build commit

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: false
-		})
+		}),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
 	}
 };
 
